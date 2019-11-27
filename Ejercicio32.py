850d60638fd47274d046ede23c6f379cc84d7f15
Default user <yalejandro.gomez@gmail.com>
2019-11-27 21:56:44 +0000

    Actualizar

--- Ejercicio32.py
+++ Ejercicio32.py
@@ -1,52 +1,35 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-difusion = np.loadtxt("difusion.dat")
+Onda = np.loadtxt("onda.dat")
 
-plt.figure(figsize=(15,4))
+fig = plt.figure(figsize=(15,4))
 
-time = difusion[:,0]
-time = np.delete(time,-1)
-x = difusion[-1,:]
-x = np.delete(x,0)
-Matrix = np.delete(difusion,0,1)
-Matrix = np.delete(Matrix,-1,0)
+time = np.linspace(0,6,np.shape(Onda)[0])
+x = np.linspace(0.0, 1.0, np.shape(data)[1])
+X, Time = np.meshgrid(x,time)
 
 plt.subplot(1,3,1)
-plt.imshow(Matrix,extent=[-1,1,1,0],aspect=2.0)
+plt.imshow(Onda,extent=[0,1,6,0],aspect=2.0)
 plt.colorbar()
-plt.xlabel('X (m)')
-plt.ylabel('Time (s)')
-plt.title("$\Psi$ ($m^2/s$)")
-
-plt.subplot(1,3,3)
-
-Psi1 = difusion[:,16]
-Psi1 = np.delete(Psi1,-1)
-
-plt.plot(time,Psi1,c='g')
-plt.grid()
-plt.xlabel('Time (s)')
-plt.ylabel('$\Psi$ ($m^2/s$)')
-plt.title("DIFUSION, x = 0m")
+plt.xlabel('X(m)')
+plt.ylabel('Time(s)')
+plt.title("Y(m)")
 
 plt.subplot(1,3,2)
+ax = plt.axes(projection='3d')
+ax.contour3D(X,Time,Onda,cmap='Viridis')
+ax.set_xlabel('X(m)')
+ax.set_ylabel('Time(s)')
+ax.set_title('Y(m)')
 
-dt = int(len(time)/10)
-col = 0 
-
-for i in range(9):
-    col += dt
-    Psi2 = difusion[col,:]
-    Psi2 = np.delete(Psi2,0)
-
-    plt.plot(x,Psi2,alpha=(i+1)/9,color='k',label='t = '+str(difusion[col,0]))
-    
+plt.subplot(1,3,3)
+plt.plot(x,Onda[0,:],c='g',label='T = 0s')
+plt.plot(x,Onda[-1,:],c='orange',label='T = 6s')
 plt.grid()
-plt.legend(loc=1)
-plt.xlabel('X (m)')
-plt.ylabel('$\Psi$ ($m^2/s$)')
-plt.title("DIFUSION")
+plt.legend()
+plt.xlabel('X(m)')
+plt.ylabel('Y(m)')
+plt.title("WAVE EQUATION")
 
-plt.savefig("difusion.png")
+plt.savefig("onda.png")
