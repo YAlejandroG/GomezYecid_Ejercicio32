4493841684587c852323b2ce50f0404bb77e2553
Default user <yalejandro.gomez@gmail.com>
2019-11-27 22:25:12 +0000

    Actualizar

--- Ejercicio32.py
+++ Ejercicio32.py
@@ -1,35 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 Onda = np.loadtxt("onda.dat")
 
-fig = plt.figure(figsize=(15,4))
+fig = plt.figure(figsize=(17,4))
 
 time = np.linspace(0,6,np.shape(Onda)[0])
 x = np.linspace(0.0, 1.0, np.shape(Onda)[1])
 X, Time = np.meshgrid(x,time)
 
-plt.subplot(1,3,1)
-plt.imshow(Onda,extent=[0,1,6,0],aspect=2.0)
-plt.colorbar()
+ax = fig.add_subplot(131,projection="3d")
+surf = ax.plot_surface(X,Time,Onda,cmap=cm.viridis,edgecolor='none')
 plt.xlabel('X(m)')
 plt.ylabel('Time(s)')
-plt.title("Y(m)")
+ax.view_init(20, 20)
+fig.colorbar(surf)
 
 plt.subplot(1,3,2)
-ax = plt.axes(projection='3d')
-ax.contour3D(X,Time,Onda,cmap='Viridis')
-ax.set_xlabel('X(m)')
-ax.set_ylabel('Time(s)')
-ax.set_title('Y(m)')
-
-plt.subplot(1,3,3)
-plt.plot(x,Onda[0,:],c='g',label='T = 0s')
-plt.plot(x,Onda[-1,:],c='orange',label='T = 6s')
+plt.plot(x,Onda[0,:],c='gold',label='T = 0s')
+plt.plot(x,Onda[-1,:],c='midnightblue',label='T = 6s')
 plt.grid()
 plt.legend()
 plt.xlabel('X(m)')
 plt.ylabel('Y(m)')
 plt.title("WAVE EQUATION")
+
+plt.subplot(1,3,3)
+plt.imshow(Onda,extent=[0,1,6,0],aspect=0.16)
+plt.colorbar()
+plt.xlabel('X(m)')
+plt.ylabel('Time(s)')
+plt.title("Y(m)")
 
 plt.savefig("onda.png")
