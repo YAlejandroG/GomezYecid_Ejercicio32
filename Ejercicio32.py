efc5ec0b4c290e69e518fc1d5771022827662eda
Default user <yalejandro.gomez@gmail.com>
2019-11-28 18:22:21 +0000

    Actualizar

--- Ejercicio32.py
+++ Ejercicio32.py
@@ -1,54 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-difusion = np.loadtxt("difusion.dat")
+Onda = np.loadtxt("onda.dat")*10**5
 
-xlab = [-1.0,-0.5,0.0,0.5,1.0]
-tlab = [1.0,0.8,0.6,0.4,0.2,0.0]
+plt.figure(figsize=(19,4))
 
-plt.figure(figsize=(15,4))
-
-time = difusion[:,0]
-time = np.delete(time,-1)
-x = difusion[-1,:]
-x = np.delete(x,0)
-Matrix = np.delete(difusion,0,1)
-Matrix = np.delete(Matrix,-1,0)
+time = np.linspace(0,0.1,np.shape(Onda)[0])
+x = np.linspace(0,1,np.shape(Onda)[1])
 
 plt.subplot(1,3,1)
-plt.imshow(Matrix,extent=[-1,1,1,0],aspect=2.0)
+plt.imshow(Onda,extent=[0,6,0.1,0],aspect=50)
 plt.colorbar()
 plt.xlabel('X (m)')
 plt.ylabel('Time (s)')
-plt.title("$\Psi$ ($m^2/s$)")
+plt.title("Y (m) $x10^5$")
 
 plt.subplot(1,3,3)
 
-Psi1 = difusion[:,16]
-Psi1 = np.delete(Psi1,-1)
+Onda1 = Onda[:,int((len(x)-1)/4)]
 
-plt.plot(time,Psi1,c='g')
+plt.plot(time,Onda1,c='g')
 plt.grid()
 plt.xlabel('Time (s)')
-plt.ylabel('$\Psi$ ($m^2/s$)')
-plt.title("DIFUSION, x = 0m")
+plt.ylabel('Y (m) $x10^5$')
+plt.title("Onda, x = 0.25m")
 
 plt.subplot(1,3,2)
 
-dt = int(len(time)/10)
+dt = int((len(time))/9)
+t = 0
 col = 0 
 
 for i in range(9):
+    t += dt*0.0001
     col += dt
-    Psi2 = difusion[col,:]
-    Psi2 = np.delete(Psi2,0)
+    Onda2 = Onda[col,:]
 
-    plt.plot(x,Psi2,alpha=(i+1)/9,color='k',label='t = '+str(difusion[col,0]))
+    plt.plot(x,Onda2,label='t = '+str(np.round(t,3)))
     
 plt.grid()
 plt.legend(loc=1)
 plt.xlabel('X (m)')
-plt.ylabel('$\Psi$ ($m^2/s$)')
-plt.title("DIFUSION")
+plt.ylabel('Y (m) $x10^5$')
+plt.title("WAVE EQUATION")
 
-plt.savefig("difusion.png")
+plt.savefig("onda.png")
